a6e9e6c784bc57846e3ab6a863cf4fce510cb55d
Leia Carter <leiacarts@gmail.com>
2018-07-30 18:42:55 -0400

    player rendered & moving on board

--- js/classes.js
+++ js/classes.js
@@ -6,20 +6,43 @@ class Avatar {
   }
 
   render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
+
+
+
 }
+
 
 class Player extends Avatar {
   constructor() {
     super();
     this.sprite += 'char-boy.png';
-      /* needs stuff to update x & y
-      @param {string} input - travel direction
-      */
-      
-    handleInput(input) {
+    this.step = 101;
+    this.jump = 83;
+    this.x = 0;
+    this.y = 0;
+      // needs stuff to update x & y
+      // @param {string} input
+      // - travel direction
+  }
 
+
+
+  handleInput(input) {
+    switch(input) {
+      case 'left':
+        this.x -= 20;
+        break;
+      case 'up':
+        this.y -= 20;
+        break;
+      case 'right':
+        this.x += 20;
+        break;
+      case 'down':
+        this.y += 20;
+        break;
     }
   }
 }
